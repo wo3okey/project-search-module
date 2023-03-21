479a51abfa8c06749b38c13c110eaf10d63dd664
wo3okey <anbriaao@naver.com>
2023-03-22 00:48:16 +0900

    chore(project): git cache clear

--- project-search-api/build/tmp/kapt3/stubs/main/com/wookey/project/search/blog/adapter/persistense/jpa/BlogSearchLogJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.wookey.project.search.blog.adapter.persistense.jpa;
-
-import java.lang.System;
-
-@kotlin.Metadata(mv = {1, 7, 1}, k = 1, d1 = {"\u0000\u0012\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0010\t\n\u0000\bf\u0018\u00002\u000e\u0012\u0004\u0012\u00020\u0002\u0012\u0004\u0012\u00020\u00030\u0001\u00a8\u0006\u0004"}, d2 = {"Lcom/wookey/project/search/blog/adapter/persistense/jpa/BlogSearchLogJpaRepository;", "Lorg/springframework/data/jpa/repository/JpaRepository;", "Lcom/wookey/project/search/blog/adapter/persistense/entity/BlogSearchLogEntity;", "", "project-search-api"})
-public abstract interface BlogSearchLogJpaRepository extends org.springframework.data.jpa.repository.JpaRepository<com.wookey.project.search.blog.adapter.persistense.entity.BlogSearchLogEntity, java.lang.Long> {
-}
